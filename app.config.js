50e7039ed8d6ad2217d192704abc70cd40b20005
Rafael Zago <rafaelszago@gmail.com>
2023-06-22 12:00:57 -0300

    feat: config dotenv

--- app.config.js
+++ app.config.js
@@ -1,9 +1,5 @@
 module.exports = ({ config }) => {
-  console.log(config.name); // prints 'My App'
   return {
     ...config,
-    extra: {
-      clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
-    },
   };
 };
