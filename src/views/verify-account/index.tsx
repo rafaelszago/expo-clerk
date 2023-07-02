1587a76901eb31a5f50535018b9a7cae76f52a05
Rafael Zago <rafaelszago@gmail.com>
2023-07-02 12:40:49 -0300

    feat: create verify account view

---
+++ src/views/verify-account/index.tsx
@@ -0,0 +1,42 @@
+import { useSignUp } from '@clerk/clerk-expo';
+import { useState } from 'react';
+import { View } from 'react-native';
+import { Button, InputGroup } from '../../components';
+
+export const VerifyAccount: React.FC = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [code, setCode] = useState('');
+
+  const onPressVerify = async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      await setActive({ session: completeSignUp.createdSessionId });
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <View className="p-8 flex flex-col justify-center min-h-screen">
+      <View>
+        <View>
+          <InputGroup
+            label="Code"
+            value={code}
+            autoCapitalize="none"
+            placeholder="Code..."
+            onChangeText={code => setCode(code)}
+          />
+        </View>
+        <Button onPress={onPressVerify}>Verify Email</Button>
+      </View>
+    </View>
+  );
+};
