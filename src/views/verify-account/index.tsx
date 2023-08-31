207d810aeed06b7a3f030354c21a76852e8ec65f
Rafael Zago <rafaelszago@gmail.com>
2023-08-31 10:39:49 -0300

    feat: navigate to dashboard after validate code

--- src/views/verify-account/index.tsx
+++ src/views/verify-account/index.tsx
@@ -2,8 +2,11 @@ import { useSignUp } from '@clerk/clerk-expo';
 import { useState } from 'react';
 import { View } from 'react-native';
 import { Button, InputGroup } from '../../components';
+import { RootStackScreenProps } from '../../protocols';
 
-export const VerifyAccount: React.FC = () => {
+export const VerifyAccount: React.FC<RootStackScreenProps<'VerifyAccount'>> = ({
+  navigation,
+}) => {
   const { isLoaded, signUp, setActive } = useSignUp();
   const [code, setCode] = useState('');
 
@@ -18,25 +21,25 @@ export const VerifyAccount: React.FC = () => {
       });
 
       await setActive({ session: completeSignUp.createdSessionId });
+
+      navigation.navigate('Dashboard');
     } catch (err: any) {
       console.log(err);
     }
   };
 
   return (
-    <View className="p-8 flex flex-col justify-center min-h-screen">
-      <View>
-        <View>
-          <InputGroup
-            label="Code"
-            value={code}
-            autoCapitalize="none"
-            placeholder="Code..."
-            onChangeText={code => setCode(code)}
-          />
-        </View>
-        <Button onPress={onPressVerify}>Verify Email</Button>
+    <View className="p-8 flex flex-col justify-center min-h-screen space-y-3">
+      <View className="mb-3">
+        <InputGroup
+          label="Code"
+          value={code}
+          autoCapitalize="none"
+          placeholder="Code..."
+          onChangeText={code => setCode(code)}
+        />
       </View>
+      <Button onPress={onPressVerify}>Verify Email</Button>
     </View>
   );
 };
