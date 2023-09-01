89b0916a8013774ebebb05c48b0ec0a8487fcc1e
Rafael Zago <rafaelszago@gmail.com>
2023-09-01 20:16:58 -0300

    feat: set firstName and lastName in sign up

--- src/views/signup/index.tsx
+++ src/views/signup/index.tsx
@@ -8,14 +8,17 @@ import {
   View,
 } from 'react-native';
 import { Button, InputGroup } from '../../components';
-import { RootStackScreenProps } from '../../protocols';
+import { ClerkError, RootStackScreenProps } from '../../protocols';
 
 export const SignUp: React.FC<RootStackScreenProps<'SignUp'>> = ({
   navigation,
 }) => {
   const { isLoaded, signUp } = useSignUp();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
 
   const onSignUpPress = async () => {
     if (!isLoaded) {
@@ -24,13 +27,18 @@ export const SignUp: React.FC<RootStackScreenProps<'SignUp'>> = ({
 
     try {
       await signUp.create({
+        firstName,
+        lastName,
         emailAddress,
         password,
       });
 
       await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
+
+      navigation.navigate('VerifyAccount');
     } catch (err) {
-      console.log(err);
+      const clerkError = err as ClerkError;
+      setErrors(clerkError.errors.map(error => error.message));
     }
   };
 
@@ -42,14 +50,31 @@ export const SignUp: React.FC<RootStackScreenProps<'SignUp'>> = ({
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <View>
         <InputGroup
+          label="First Name"
+          autoCapitalize="words"
+          value={firstName}
+          placeholder="John"
+          onChangeText={firstName => setFirstName(firstName)}
+        />
+      </View>
+      <View>
+        <InputGroup
+          label="Last Name"
+          autoCapitalize="words"
+          value={lastName}
+          placeholder="Doe"
+          onChangeText={lastName => setLastName(lastName)}
+        />
+      </View>
+      <View>
+        <InputGroup
           label="E-mail"
           autoCapitalize="none"
           value={emailAddress}
-          placeholder="Email..."
-          onChangeText={email => setEmailAddress(email)}
+          placeholder="you@mail.com"
+          onChangeText={emailAddress => setEmailAddress(emailAddress)}
         />
       </View>
-
       <View className="mb-3">
         <InputGroup
           label="Password"
@@ -59,9 +84,13 @@ export const SignUp: React.FC<RootStackScreenProps<'SignUp'>> = ({
           onChangeText={password => setPassword(password)}
         />
       </View>
-
       <Button onPress={onSignUpPress}>Sign up</Button>
-
+      {errors.length > 0 &&
+        errors.map((error, index) => (
+          <Text key={index} className="text-sm text-red-500">
+            {error}
+          </Text>
+        ))}
       <View className="flex flex-row gap-1">
         <Text>Have an account?</Text>
         <TouchableOpacity onPress={onSignInPress}>
